ed224525103a6ab88574e7a12f5df1a500c733a1
Javier-VL <javiervelazquezlozano@gmail.com>
2021-05-08 03:00:28 -0500

    sub nav agregada

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,6 +6,9 @@ import NavsBS from '../components/NavsBS'
 import All_Screen from '../components/content/All_Screen'
 import Sofas_Screen from '../components/content/Sofas_Screen'
 import NavsBS2 from '../components/NavsBS2'
+import Guardado_Screen from '../components/content/Guardado_Screen';
+import Mesas_Screen from '../components/content/Mesas_Screen';
+import Sillas_Screen from '../components/content/Sillas_Screen';
 
 
 
@@ -18,6 +21,10 @@ export default function AppRouter() {
                 <Switch>
                     <Route exact path="/" component={All_Screen}/>
                     <Route exact path="/sofas" component={Sofas_Screen}/>
+                    <Route exact path="/guardado" component={Guardado_Screen}/>
+                    <Route exact path="/mesas" component={Mesas_Screen}/>
+                    <Route exact path="/sillas" component={Sillas_Screen}/>
+                    
                 </Switch>
 
            </div>
