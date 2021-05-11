5bbe63e1a59cb57867c7343039d33c79af986bcb
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-05-11 15:11:28 -0500

    Diseño del navbar

--- src/components/NavbarBS.js
+++ src/components/NavbarBS.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import { Navbar,Nav,NavDropdown } from 'react-bootstrap'
+import { Dropdown } from 'bootstrap';
+import React,{useState} from 'react'
+import { Navbar,Nav,NavDropdown, Form, Search, InputGroup, Button } from 'react-bootstrap'
 
 export default function NavbarBS() {
+
+    //Para mostrar cuando pase el cursor
+    const [show, setShow] = useState(false)
+
+    const showDropdown = (e) => {
+        setShow(!show);
+    }
+
+    const hideDropdown = (e) => {
+        setShow(false);
+    }
+
     return (
         <React.Fragment>
-            <Navbar className="container-fluid"   collapseOnSelect expand="lg" bg="transparent"  variant="light" >
-                <Navbar.Brand href="#home">Interior Forma</Navbar.Brand>
+            <Navbar className="bg-light justify-content-between"   collapseOnSelect expand="lg" bg="transparent"  variant="light" >
+                <Navbar.Brand href="#home">Euroespacio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" className="navbar-toggler"></Navbar.Toggle>
                 
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavDropdown title="Productos" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Sofas</NavDropdown.Item>
-                            <NavDropdown.Divider />
+                        <NavDropdown title="Productos" id="collasible-nav-dropdown" show={show} onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+                            <NavDropdown  title="Profile" id="collasible-nav-dropdown-right" />
                             <NavDropdown.Item href="#action/3.2">Sillas</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.3">Guardado</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Mesas</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#pricing">Contacto</Nav.Link>
-                        
-
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">Info</Nav.Link>
- 
                     </Nav>
                 </Navbar.Collapse>
+
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Form.Control type="text" placeholder="Buscar" />
+                    <Button variant="light">Buscar</Button>
+                </Navbar.Collapse>
+
+                <Navbar.Collapse className="justify-content-end">
+                    <NavDropdown title="Contacto" id="collasible-nav-dropdown">
+                        <NavDropdown.Item>Ubicación</NavDropdown.Item>
+                        <NavDropdown.Item>Información</NavDropdown.Item>
+                    </NavDropdown>
+                </Navbar.Collapse>
+
             </Navbar>
         </React.Fragment>
     )
