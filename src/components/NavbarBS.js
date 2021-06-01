4b5278bb271f92bf5dc49d4d2446f9017dfe9518
Javier-VL <javiervelazquezlozano@gmail.com>
2021-05-31 20:10:13 -0500

    fichas tecnicas agregadas

--- src/components/NavbarBS.js
+++ src/components/NavbarBS.js
@@ -1,6 +1,6 @@
 import { Dropdown } from 'bootstrap';
-import React,{useState} from 'react'
-import { Navbar,Nav,NavDropdown, Form, Search, InputGroup, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Navbar, Nav, NavDropdown, Form, Search, InputGroup, Button } from 'react-bootstrap'
 
 
 export default function NavbarBS() {
@@ -10,35 +10,54 @@ export default function NavbarBS() {
 
     const showDropdown = (e) => {
         setShow(!show);
+        window.scroll({
+            top: document.body.offsetHeight,
+            left: 0, 
+            behavior: 'smooth',
+          });
     }
 
     const hideDropdown = (e) => {
         setShow(false);
     }
 
+    const handleScroll = ()=> {
+        window.scroll({
+            top: document.body.offsetHeight,
+            left: 0, 
+            behavior: 'smooth',
+          });
+    }
+
     return (
         <React.Fragment>
-            <Navbar className=" bg-light justify-content-between"   collapseOnSelect expand="lg" bg="transparent"  variant="light" >
+            <Navbar className=" bg-light justify-content-between" collapseOnSelect expand="lg" bg="transparent" variant="light" >
                 <Navbar.Brand href="#home">‎‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎InteriorForma</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" className="navbar-toggler"></Navbar.Toggle>
-                
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavDropdown title="Productos" id="collasible-nav-dropdown" show={show} onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
-                            <NavDropdown  title="Profile" id="collasible-nav-dropdown-right" />
-                            <NavDropdown.Item href="#action/3.2">Sillas</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.3">Guardado</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Mesas</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
 
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Form.Control type="text" placeholder="Buscar" />
-                    <Button variant="light">Buscar</Button>
-                </Navbar.Collapse>
+                
+
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <NavDropdown title="Productos" id="collasible-nav-dropdown" show={show} onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+                                <NavDropdown title="Profile" id="collasible-nav-dropdown-right" />
+                                <NavDropdown.Item href="#action/3.2">Sillas</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">Guardado</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Mesas</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                  
+                        <Form.Control className="form-control mr-sm-2" type="text" placeholder="Buscar" />
+                        <Button variant="btn btn-secondary active" onClick={showDropdown}>Buscar</Button>
+                       
+                    </Navbar.Collapse>
+                
 
                 <Navbar.Collapse className="justify-content-end">
                     <NavDropdown title="Contacto" id="collasible-nav-dropdown">
